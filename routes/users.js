3368c9eba0c82755a386871d9cb23a5f2b93eaf2
cleomaster <cleo.master0000@gmail.com>
2022-12-17 16:46:13 +0500

    Add files via upload

--- routes/users.js
+++ routes/users.js
@@ -17,7 +17,7 @@ router.post("/login", async (req, res) => {
     }) || {}
 
     if (!user) return res.send("Invalid username or password")
-    const token = jwt.sign({user_id: user.user_id, name: user.name, email: user.email}, 'jwtPrivateKey');
+    const token = jwt.sign({user_id: user.user_id, name: user.name, email: user.email}, process.env.JWT || 'jwtPrivateKey');
     res.header('x-auth-token', token).header("access-control-expose-headers", "x-auth-token").send({user_id: user.user_id, email: user.email, name: user.name});
 })
 
