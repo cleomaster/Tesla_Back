4c3e232f7a9cdb12e4945389c1d7cbc9dd7d4561
cleomaster <cleo.master0000@gmail.com>
2022-12-17 16:48:35 +0500

    Add files via upload

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -5,7 +5,7 @@ function auth(req, res, next) {
     if (!token) return res.status(401).send("No token provided");
 
     try {
-        const decoded = jwt.verify(token, process.env.JWT || "jwtPrivateKey");
+        const decoded = jwt.verify(token, process.env.JWT);
         req.user = decoded;
         next();
     } catch (ex) {
